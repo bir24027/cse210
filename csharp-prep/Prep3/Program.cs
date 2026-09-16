c73affbbb0c94d700a8a2f69fc5654b2c75f00d6
birchwillr-code <birchwillr@gmail.com>
2026-09-16 17:05:16 -0600

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Globalization;
+using System.Runtime.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator=new Random();
+        string continuePlay;
+        do
+        {
+        int number=randomGenerator.Next(0,100);
+        int guess;
+        string answer;
+        int guesses=0;
+        do
+        {
+            guesses++;
+            Console.Write("What is your guess? ");
+            answer=Console.ReadLine();
+            guess=int.Parse(answer);
+            if (guess>number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess<number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess==number)
+            {
+                Console.WriteLine("Your guess is correct!");
+            }
+        }while(guess!=number);
+        Console.WriteLine($"You guessed {guesses} times.");
+        Console.Write("Do you want to play again?");
+        continuePlay=Console.ReadLine();
+        }while (continuePlay=="Yes");
     }
 }
